6f69a77946207bfaaf733b0619d48bb63c24d46f
April Dagonese <adagonese@gmail.com>
2019-01-02 14:33:35 -0700

    Completes Iteration 3

--- lib/curator.rb
+++ lib/curator.rb
@@ -24,6 +24,29 @@ class Curator
     @photographs.find do |photo|
       photo.id == id
     end
+  end
+
+  def find_photographs_by_artist(artist)
+    @photographs.select do |photo|
+      photo.artist_id == artist.id
+    end
+  end
+
+  def artists_with_multiple_photographs
+    @artists.select do |artist|
+      find_photographs_by_artist(artist).count > 1
+    end
+  end
+
+  def photographs_taken_by_artist_from(country)
+    photos = []
+    @photographs.each do |photo|
+      if find_artist_by_id(photo.artist_id).country == country
+        photos << photo
+      end
+    end
+    photos
   end 
+
 
 end
--- test/curator_test.rb
+++ test/curator_test.rb
@@ -19,6 +19,16 @@ class CuratorTest < Minitest::Test
       name: "Moonrise, Hernandez",
       artist_id: "2",
       year: "1941" }
+    @photo_3 = {
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967" }
+    @photo_4 = {
+      id: "4",
+      name: "Child with Toy Hand Grenade in Central Park",
+      artist_id: "3",
+      year: "1962" }
     @artist_1 = {
       id: "1",
       name: "Henri Cartier-Bresson",
@@ -30,6 +40,12 @@ class CuratorTest < Minitest::Test
       name: "Ansel Adams",
       born: "1902",
       died: "1984",
+      country: "United States" }
+    @artist_3 = {
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
       country: "United States" }
   end
 
@@ -78,11 +94,64 @@ class CuratorTest < Minitest::Test
   def test_it_finds_photograph_by_id
     @curator.add_photograph(@photo_1)
     @curator.add_photograph(@photo_2)
-    
+
     moonrise = @curator.find_photograph_by_id("2")
 
     assert_instance_of Photograph, moonrise
     assert_equal "Moonrise, Hernandez", moonrise.name
   end
+
+  def test_it_finds_photographs_by_artist
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    diane_arbus = @curator.find_artist_by_id("3")
+    photo3 = @curator.find_photograph_by_id("3")
+    photo4 = @curator.find_photograph_by_id("4")
+
+    assert_equal [photo3, photo4], @curator.find_photographs_by_artist(diane_arbus)
+  end
+
+  def test_it_finds_artists_with_multiple_photographs
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    diane_arbus = @curator.find_artist_by_id("3")
+
+    assert_equal [diane_arbus], @curator.artists_with_multiple_photographs
+    assert_equal 1, @curator.artists_with_multiple_photographs.count
+  end
+
+  def test_it_finds_photographs_by_artist_country
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    photo1 = @curator.find_photograph_by_id("1")
+    photo2 = @curator.find_photograph_by_id("2")
+    photo3 = @curator.find_photograph_by_id("3")
+    photo4 = @curator.find_photograph_by_id("4")
+
+    assert_equal [photo2, photo3, photo4], @curator.photographs_taken_by_artist_from("United States")
+    assert_equal [photo1], @curator.photographs_taken_by_artist_from("France")
+    assert_equal [], @curator.photographs_taken_by_artist_from("Argentina")
+  end
+
+  def test_it_loads_photographs
+  end
+
+  def test_it_loads_artists
+  end 
 
 end
